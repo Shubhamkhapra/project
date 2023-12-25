f3e6d274656c0b32b45445ab2d85d8f92ce4a593
shubham khapra <shubhamkhapra21@gmail.com>
2023-12-26 00:00:27 +0530

    add travis .yaml add

---
+++ config/ci.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID:
+      '1024150306667-urjherhqs5unq723d1t7eogsf66qklt4.apps.googleusercontent.com',
+    googleClientSecret: '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',
+    mongoURI:"mongodb://127.0.0.1:27017/blog_ci",
+    cookieKey: '123123123',
+  };
